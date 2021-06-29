7b93949ff5b6e3ba4baeb6a966dd187b31b4599d
Hugo Gicquel <hugogicquel@me.com>
2021-06-29 11:30:39 +0200

    ex09

---
+++ ex07/r_dwssap.sh
@@ -0,0 +1,1 @@
+cat /etc/passwd | grep -v '#' | awk 'NR%2<2' | awk -F ':' '{print $1}' | rev | sort -r | head -n ${FT_LINE} | tail -n ${FT_LINE2} | awk 'ORS=", "' | rev | cut -c 3- | rev | sed "s/$/./"
